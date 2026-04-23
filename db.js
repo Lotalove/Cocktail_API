9a6926636e2b96bba4c595cbe20c800ed53a0cb7
unknown <capisceteddy@gmail.com>
2026-04-23 16:51:31 -0400

    added ssl in hopes to fix prod

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 import postgres from 'postgres'
 
 const connectionString = process.env.DATABASE_URL
-const sql = postgres(connectionString)
+const sql = postgres(connectionString,{ssl:"require",max:1})
 
 export default sql
